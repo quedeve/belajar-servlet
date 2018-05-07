5ab04b49a4e5939e6e2fec4400654f88015630e5
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2018-05-07 16:16:07 +0700

    servlet redirect

--- src/main/java/com/tabeldata/servlet/example/ServletRedirect.java
+++ src/main/java/com/tabeldata/servlet/example/ServletRedirect.java
@@ -14,9 +14,11 @@ public class ServletRedirect extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 //        super.doPost(req, resp);
-        String nama = req.getParameter("nama");
-        req.setAttribute("namaLengkap", nama);
-        RequestDispatcher rd = req.getRequestDispatcher("/belajar.jsp");
+
+        req.setAttribute("namaLengkap", req.getParameter("nama"));
+        req.setAttribute("umur", req.getParameter("umur"));
+        req.setAttribute("kelas", req.getParameter("kelas"));
+        RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/belajar.jsp");
         rd.forward(req, resp);
     }
 }
