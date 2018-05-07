0e590ab661c4512f9bb7844f3408ead912ef8fb1
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2018-05-07 14:18:12 +0700

    belajar html tag & atribut

--- src/main/java/com/tabeldata/servlet/example/ContohServlet.java
+++ src/main/java/com/tabeldata/servlet/example/ContohServlet.java
@@ -12,7 +12,7 @@ public class ContohServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 //        super.doGet(req, resp);
         String namaLengkap = req.getParameter("nama");
-        String nim = req.getParameter("nim");
+        String nim = req.getParameter("NPM");
         resp.getWriter().append("Halo saya sedang belajar servlet, nama saya adalah " + namaLengkap + " dan nimnya adalah " + nim);
     }
 }
