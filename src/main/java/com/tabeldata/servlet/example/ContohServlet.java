f4ad3474e4060c0dc3035494a966ec7ed87182c1
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2018-05-07 11:45:56 +0700

    mengirin data dengan query param

--- src/main/java/com/tabeldata/servlet/example/ContohServlet.java
+++ src/main/java/com/tabeldata/servlet/example/ContohServlet.java
@@ -1,7 +1,18 @@
 package com.tabeldata.servlet.example;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class ContohServlet extends HttpServlet {
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+        String namaLengkap = req.getParameter("nama");
+        String nim = req.getParameter("nim");
+        resp.getWriter().append("Halo saya sedang belajar servlet, nama saya adalah " + namaLengkap + " dan nimnya adalah " + nim);
+    }
 }
